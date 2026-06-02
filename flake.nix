7e9aad598c4a8e0230062c87d3baf8fe2a339877
loner <2788892716@qq.com>
2026-06-03 02:41:37 +0800

    disable gha

--- flake.nix
+++ flake.nix
@@ -25,70 +25,81 @@
 
   outputs =
     { self, nixpkgs, ... }@inputs:
-    let
-      eachSystem =
-        accu: system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        accu
-        // {
-          # Exposes the packages created by the overlay.
-          legacyPackages = (accu.legacyPackages or { }) // {
-            ${system} = accu.utils.applyOverlay { inherit pkgs; };
-          };
-          packages = (accu.packages or { }) // {
-            ${system} = accu.utils.applyOverlay {
-              inherit pkgs;
-              onlyDerivations = true;
-            };
-          };
+    builtins.trace
+      ''
+        nyx-loner is deprecated.
 
-          # Needed to build without impure
-          unrestrictedPackages = (accu.unrestrictedPackages or { }) // {
-            ${system} = accu.utils.applyOverlay {
-              pkgs = import nixpkgs {
-                inherit system;
-                config = {
-                  allowUnfree = true;
-                  allowUnsupportedSystem = true;
-                  nvidia.acceptLicense = true;
+        Upstream chaotic-cx/nyx is actively maintained again.
+
+        Please migrate to:
+          github:chaotic-cx/nyx
+      ''
+      (
+        let
+          eachSystem =
+            accu: system:
+            let
+              pkgs = nixpkgs.legacyPackages.${system};
+            in
+            accu
+            // {
+              # Exposes the packages created by the overlay.
+              legacyPackages = (accu.legacyPackages or { }) // {
+                ${system} = accu.utils.applyOverlay { inherit pkgs; };
+              };
+              packages = (accu.packages or { }) // {
+                ${system} = accu.utils.applyOverlay {
+                  inherit pkgs;
+                  onlyDerivations = true;
                 };
               };
+
+              # Needed to build without impure
+              unrestrictedPackages = (accu.unrestrictedPackages or { }) // {
+                ${system} = accu.utils.applyOverlay {
+                  pkgs = import nixpkgs {
+                    inherit system;
+                    config = {
+                      allowUnfree = true;
+                      allowUnsupportedSystem = true;
+                      nvidia.acceptLicense = true;
+                    };
+                  };
+                };
+              };
+
+              # I would prefer if we had something stricter, with attribute alphabetical
+              # sorting, and optimized for git's diffing. But this is the closer we have.
+              formatter = (accu.formatter or { }) // {
+                ${system} = import ./maintenance/formatter.nix pkgs;
+              };
             };
+
+          universals = {
+            # To fix `nix show` and FlakeHub
+            schemas = import ./maintenance/schemas { flakes = inputs; };
+
+            # The stars: our overlay and our modules.
+            overlays.default = import ./overlays { flakes = inputs; };
+            overlays.cache-friendly = import ./overlays/cache-friendly.nix { flakes = inputs; };
+            nixosModules = import ./modules/nixos { flakes = inputs; };
+            homeModules = import ./modules/home-manager { flakes = inputs; };
+            homeManagerModules = self.homeModules;
+
+            # Dev stuff.
+            utils = import ./shared/utils.nix {
+              nyxOverlay = self.overlays.default;
+              inherit (nixpkgs) lib;
+            };
+            inherit (import ./flake.nix) nixConfig;
           };
-
-          # I would prefer if we had something stricter, with attribute alphabetical
-          # sorting, and optimized for git's diffing. But this is the closer we have.
-          formatter = (accu.formatter or { }) // {
-            ${system} = import ./maintenance/formatter.nix pkgs;
-          };
-        };
-
-      universals = {
-        # To fix `nix show` and FlakeHub
-        schemas = import ./maintenance/schemas { flakes = inputs; };
-
-        # The stars: our overlay and our modules.
-        overlays.default = import ./overlays { flakes = inputs; };
-        overlays.cache-friendly = import ./overlays/cache-friendly.nix { flakes = inputs; };
-        nixosModules = import ./modules/nixos { flakes = inputs; };
-        homeModules = import ./modules/home-manager { flakes = inputs; };
-        homeManagerModules = self.homeModules;
-
-        # Dev stuff.
-        utils = import ./shared/utils.nix {
-          nyxOverlay = self.overlays.default;
-          inherit (nixpkgs) lib;
-        };
-        inherit (import ./flake.nix) nixConfig;
-      };
-    in
-    builtins.foldl' eachSystem universals [
-      "x86_64-linux"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
+        in
+        builtins.foldl' eachSystem universals [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ]
+      );
 
   # Allows the user to use our cache when using `nix run <thisFlake>`.
   nixConfig = {
